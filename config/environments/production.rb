5cd9ba59c21029b9a33d96843570ad51d5c90399
Clementina Mendes <clementinadesmen@gmail.com>
2022-11-30 18:10:52 +0000

    Send mail with the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,21 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['EMAIL_USERNAME'],
+    password:             ENV['SECRET_KEY'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Settings specified here will take precedence over those in config/application.rb.
+
+  config.hosts << ENV['HOSTNAME']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -66,7 +80,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'pages#home'
+  get '/contact', to: 'contacts#new'
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :contacts, only: [:new, :create]
+  resources :users
 end
